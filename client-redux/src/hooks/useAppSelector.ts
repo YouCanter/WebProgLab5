9cd9d9cdbde4e401aac4fec59d5431d856ddc9ce
YouCanter <egorfedorov0306@gmail.com>
2026-05-05 02:20:19 +0300

    Task 1

--- client-redux/src/hooks/useAppSelector.ts
+++
@@ -1,4 +0,0 @@
-import { useSelector, type TypedUseSelectorHook } from "react-redux";
-import type { RootState } from "@/app/store";
-
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
